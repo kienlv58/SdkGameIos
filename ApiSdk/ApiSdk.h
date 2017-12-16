8d068ac3935d81996ff7c36adbb09fd24468b6bc
Duong Van Phuc <phuc.duong@nccsoft.vn>
2017-12-16 13:46:23 +0700

    Initial Commit

---
+++ ApiSdk/ApiSdk.h
@@ -0,0 +1,19 @@
+//
+//  ApiSdk.h
+//  ApiSdk
+//
+//  Created by Nguyen Tran Nhan on 12/16/17.
+//  Copyright © 2017 nccsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApiSdk.
+FOUNDATION_EXPORT double ApiSdkVersionNumber;
+
+//! Project version string for ApiSdk.
+FOUNDATION_EXPORT const unsigned char ApiSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApiSdk/PublicHeader.h>
+
+
